d6f57f0c53de9bdbd86ff5be88d3b1d9290e05de
Juan Reon Badenhorst <juanrbadenhorst@gmail.com>
2024-07-02 15:34:48 +0200

    Chapter 6

---
+++ Chapter 6/index.js
@@ -0,0 +1,72 @@
+// let message = "Hello world!";
+// console.log(message);
+// let myname = "Juan";
+// console.log("Hello " + myname);
+// if (myname.length > 7) {
+//  console.log("Wow you have a really long name");
+// }
+// else{
+//     console.log("Your name is not very long");
+// }
+
+// let lemonChicken = false;
+// let beefWithBlackBean = false;
+// let sweetAndSourPork = false;
+
+// if (lemonChicken) {
+// console.log("Great! I'm having lemon chicken!");
+// } else if (beefWithBlackBean) {
+// console.log("I'm having the beef.");
+// } else if (sweetAndSourPork) {
+// console.log("OK, I'll have the pork.");
+// } else {
+// console.log("Well, I guess I'll have rice then.");
+// }
+
+// let Juan = false;
+// let Mom = false;
+// let Dad = true;
+
+// if (Juan) {   
+// console.log("Hello Me");
+// } else if (Mom){
+//     console.log("Hi Mom");
+// } else if (Dad) {
+//     console.log("Hi Dad");
+// } else {
+//     console.log("Hello stranger")
+// }s
+
+// let sheepCounted= prompt("Counted Sheep")
+
+// while (prompt <= 10){
+//  console.log("I have counted" + sheepCounted);
+// };
+// console.log("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz..................")
+
+// for (let sheepCounted = 0; sheepCounted <= 10; sheepCounted++){
+//     console.log("I have counted " + sheepCounted + " sheep")
+// };
+// if (sheepCounted = 10){
+//     console.log("GO TO SLEEP")
+// }
+
+// let timeToSayHello = 3;
+// for(let i = 0;i < timeToSayHello; i++ ){
+//     console.log("Hello")
+// }
+
+
+// let animalsNames=prompt("Name a animal");
+// let animals = (animalsNames) 
+
+// for (let i = 0; i < animals.length; i++) {
+// console.log("This zoo contains a " + animals[i] + ".");
+// // }
+// var animals = ["Lion", "Flamingo", "Polar Bear", "Boa Constrictor"];
+// for (var i = 0; i < animals.length; i++) {
+// console.log("This zoo contains a " + animals[i] + ".");
+// }
+//  for (let x = 2; x < 10000; x=x*2){
+//     console.log(x);
+//  }
